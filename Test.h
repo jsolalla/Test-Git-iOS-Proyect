9ff108992840183867aeff6d8c2b775674bd0947
iGeesus <Mithrandir@Jesuss-MacBook-Pro.local>
2014-09-14 20:27:27 -0500

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Jesús Santa Olalla on 9/14/14.
+//  Copyright (c) 2014 Jesús Santa Olalla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Jesús Santa Olalla on 9/14/14.
+//  Copyright (c) 2014 Jesús Santa Olalla. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
